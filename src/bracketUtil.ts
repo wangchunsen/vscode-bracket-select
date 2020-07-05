aaa12adab104713ef0cf44ced443d362b8df11bc
FuPeiJiang <42662615+FuPeiJiang@users.noreply.github.com>
2020-07-05 11:44:12 -0400

    allow custom bracket types (in settings)
    1)renamed "quoteBrackets" to "SameBracket" to help understand when changing settings

--- src/bracketUtil.ts
+++ src/bracketUtil.ts
@@ -1,30 +1,27 @@
 'use strict';
 
 export namespace bracketUtil {
-    let bracketParis = [
-        ["(", ")"], 
-        ["{", "}"], 
-        ["[", "]"]
-    ]
 
-    let quoteBrackets = ['"', "'", "`"]
+    const vs = require("vscode");
+    let bracketPairs = vs.workspace.getConfiguration('bracket-select').get('bracketPairs');
+    let sameBracket = vs.workspace.getConfiguration('bracket-select').get('sameBracket');
 
     export function isMatch(open: string, close: string): Boolean {
-        if (isQuoteBracket(open)) {
+        if (isSameBracket(open)) {
             return open === close;
         } 
-        return bracketParis.findIndex(p => p[0] === open && p[1] === close) >= 0;
+        return bracketPairs.findIndex(p => p[0] === open && p[1] === close) >= 0;
     }
 
     export function isOpenBracket(char: string): Boolean {
-        return bracketParis.findIndex(pair => pair[0] === char) >= 0;
+        return bracketPairs.findIndex(pair => pair[0] === char) >= 0;
     }
 
     export function isCloseBracket(char: string): Boolean {
-        return bracketParis.findIndex(pair => pair[1] === char) >= 0;
+        return bracketPairs.findIndex(pair => pair[1] === char) >= 0;
     }
 
-    export function isQuoteBracket(char: string): Boolean {
-        return quoteBrackets.indexOf(char) >= 0;
+    export function isSameBracket(char: string): Boolean {
+        return sameBracket.indexOf(char) >= 0;
     }
 }
