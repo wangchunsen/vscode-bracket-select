1161ce5a52900804c87f585a95aeb4330e67c73a
ljonaisak-samsung <119618097+l-jonasiak@users.noreply.github.com>
2023-08-30 15:45:54 +0200

    Update bracketUtil.ts

--- src/bracketUtil.ts
+++ src/bracketUtil.ts
@@ -1,7 +1,7 @@
 'use strict';
 
 export namespace bracketUtil {
-    let bracketParis = [
+    let bracketPairs = [
         ["(", ")"], 
         ["{", "}"], 
         ["[", "]"]
@@ -13,15 +13,15 @@ export namespace bracketUtil {
         if (isQuoteBracket(open)) {
             return open === close;
         } 
-        return bracketParis.findIndex(p => p[0] === open && p[1] === close) >= 0;
+        return bracketPairs.findIndex(p => p[0] === open && p[1] === close) >= 0;
     }
 
     export function isOpenBracket(char: string): Boolean {
-        return bracketParis.findIndex(pair => pair[0] === char) >= 0;
+        return bracketPairs.findIndex(pair => pair[0] === char) >= 0;
     }
 
     export function isCloseBracket(char: string): Boolean {
-        return bracketParis.findIndex(pair => pair[1] === char) >= 0;
+        return bracketPairs.findIndex(pair => pair[1] === char) >= 0;
     }
 
     export function isQuoteBracket(char: string): Boolean {
